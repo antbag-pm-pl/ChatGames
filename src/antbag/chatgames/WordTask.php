56743790f5e3eb4df8897e93ba7a22a908654fc9
notant <84870445+antbag-dev@users.noreply.github.com>
2023-07-29 16:56:05 +0500

    Update WordTask.php

--- src/antbag/chatgames/WordTask.php
+++ src/antbag/chatgames/WordTask.php
@@ -6,7 +6,10 @@ use pocketmine\scheduler\Task;
 use antbag\chatgames\Main;
 
 class WordTask extends Task {
-  
+
+  public function __construct(){
+        
+    }
   public function onRun() : void {
     Main::getInstance()->scrambleWord();
   }
