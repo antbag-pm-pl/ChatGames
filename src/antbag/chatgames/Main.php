a87398366b23631db3f9e24b2f51b1288cd696b9
notant <84870445+antbag-dev@users.noreply.github.com>
2023-08-05 17:29:23 +0500

    Update Main.php

--- src/antbag/chatgames/Main.php
+++ src/antbag/chatgames/Main.php
@@ -7,6 +7,14 @@ use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\Listener;
 use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
 
+ use pocketmine\player\Player; 
+ use pocketmine\Server;
+ use pocketmine\event\Listener;
+ use pocketmine\event\player\PlayerJoinEvent;
+
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
+
 class Main extends PluginBase implements Listener{
 
     public ?string $word = null;
@@ -15,8 +23,15 @@ class Main extends PluginBase implements Listener{
 
     public function onEnable() : void {
         if (!$this->getServer()->getPluginManager()->getPlugin("BedrockEconomy")) {
-            $this->getLogger()->warning("Reward has been disabled since you do not have BedrockEconomy installed on your server.");  
+            $this->getLogger()->warning("Reward has been disabled since you do not have BedrockEconomy installed on your server.");
         }
+        @mkdir($this->getDataFolder() . "topten_data");
+		$this->saveResource("setting.yml");
+		$this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML))->getAll();
+		if(empty($this->config["positions"])){
+			$this->getServer()->getLogger()->Info("Please Set Location");
+			return;
+        
         $this->loadWords();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getScheduler()->scheduleDelayedTask(new WordTask($this), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
@@ -42,10 +57,68 @@ class Main extends PluginBase implements Listener{
         }
     }
     public function rewardPlayer($player)  {
+      $name = $player->getName();
       $this->getServer()->broadcastMessage("§6" . $player->getName() . " Guessed The Word Correctly.\n§6The Word Was §e" . $this->word);
-        BedrockEconomyAPI::legacy()->addToPlayerBalance($player, $this->reward);
+      BedrockEconomyAPI::legacy()->addToPlayerBalance($player, $this->reward);
+      $data = new Config($this->getDataFolder() . "topten_data/top.yml", Config::YAML);
+	  $up = $data->get($name);
+	  $data->set($name, $up + 1);
+	  $data->save();
         
     }
+
+    public function createtopten(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+		$w = $this->getConfig()->get("world");
+		$world = $player->getWorld()->getDisplayName() === "$w";
+		$top = $this->getConfig()->get("enable");	
+
+		if($world){
+			if($top == "true"){
+				$this->getLeaderBoard();
+			}
+		}
+	}
+
+    public function settopdata(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+		$name = $player->getName();		
+
+		$farm = new Config($this->getDataFolder() . "topten_data/top.yml", Config::YAML);
+		if(!$farm->exists($name)){
+			$farm->set($name, 0);
+			$farm->save();
+		}
+	}
+	
+	public function getLeaderBoard(): string{
+    $data = new Config($this->getDataFolder() . "topten_data/top.yml", Config::YAML);
+    $setting = new Config($this->getDataFolder() . "setting.yml", Config::YAML);
+    $swallet = $data->getAll();
+    $message = "";
+    $top = $setting->get("title-lb");
+    
+    if (count($swallet) > 0) {
+        arsort($swallet);
+        $i = 1;
+        foreach ($swallet as $name => $amount) {
+            $tags = str_replace(["{num}", "{player}", "{amount}"], [$i, $name, $amount], $setting->get("text-lb")) . "\n";
+            $message .= "\n ".$tags;
+            
+            if ($i >= 10) {
+                break;
+            }
+            ++$i;
+        }
+    }
+	$return = (string) $top.$message;
+    	return $return;
+}
+
+	public function getParticles(): array{
+		return $this->particle;
+	}
+        
 
     public function scrambleWord() {
         $this->word = $this->words[array_rand($this->words)];
@@ -55,6 +128,7 @@ class Main extends PluginBase implements Listener{
         }
         $this->getScheduler()->scheduleDelayedTask(new WordTask($this), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
     }
+        
     public static function getInstance(): Main{
         return self::$instance;
     }
